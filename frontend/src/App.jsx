d3ddfbb9f115d4635d32cfb9ea7197b97c9b1f9f
rshvraj <rrishabh.flutter@gmail.com>
2023-09-30 04:33:30 +0530

    Pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import {BroserRouter, Routes, Route} from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 
 export default function App() {
-  return (
-<h1 className='text-red-500'>App</h1> 
- )
+  return <BroserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    <Route path="/about" element={<About/>}/>
+
+  </Routes>
+  </BroserRouter>
 }
