badf3f2f6b91c713c6c5166df594b30fa623b351
rshvraj <rrishabh.flutter@gmail.com>
2023-09-30 19:25:48 +0530

    connected to databse

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express= require("express")
 const app=express();
+const{connection}=require("./config/db");
+app.use(express.json());
 
-app.listen(8080, ()=>{
-    console.log("Listening on port 8080")
-})
+const PORT=8080
+app.listen(PORT, async()=>{
+    try{
+        await connection
+        console.log("connected to db")
+    }
+    catch(e){
+        console.log("Error connecting to DB:",e)
+    }
+    console.log(`listening on PORT ${PORT}`
+    )})
