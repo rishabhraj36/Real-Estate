2a034004a2017de5ce2bff816f906b25e94ee12e
rshvraj <rrishabh.flutter@gmail.com>
2023-09-30 19:32:49 +0530

    Revert "connected to databse"
    This reverts commit badf3f2f6b91c713c6c5166df594b30fa623b351.

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,6 @@
 const express= require("express")
 const app=express();
-const{connection}=require("./config/db");
-app.use(express.json());
 
-const PORT=8080
-app.listen(PORT, async()=>{
-    try{
-        await connection
-        console.log("connected to db")
-    }
-    catch(e){
-        console.log("Error connecting to DB:",e)
-    }
-    console.log(`listening on PORT ${PORT}`
-    )})
+app.listen(8080, ()=>{
+    console.log("Listening on port 8080")
+})
