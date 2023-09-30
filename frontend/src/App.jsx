00d933246b25038bf9f36814b3b6ee5fe8e56f94
rshvraj <rrishabh.flutter@gmail.com>
2023-09-30 12:58:28 +0530

    header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,10 +24,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Headers from './componets/Headers';
 
  const App= () => {
   return (
     <BrowserRouter>
+    <Headers/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
---
+++ frontend/src/componets/Headers.jsx
@@ -0,0 +1,27 @@
+import {FaSearch} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Headers = () => {
+  return (
+    <header className='bg-slate-200 shadow-md' >
+    <div className='flex justify-between items-center max-w-6xl max-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
+            <span className='text-slate-500'>REAL</span>
+            <span className='text-slate-700'>ESTATE</span>
+        </h1>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-centre'>
+            <input type="text" placeholder='Search..' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+            <FaSearch/>
+        </form>
+        <ul className='flex gap-4'>
+            <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
+            <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
+            <Link to='/sign-in'><li className='hidden sm:inline text-slate-700 hover:underline'>Sign</li></Link>
+         </ul>
+    </div>
+     
+    </header>
+    )
+}
+
+export default Headers
