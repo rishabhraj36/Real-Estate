f82ddb16d6b769d6c4853a15cb3d9756ec40d639
rshvraj <rrishabh.flutter@gmail.com>
2023-10-01 21:56:47 +0530

    redux toolkit added

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart,signInSuccess,signInFailure} from '../redux/user/userSlice';
 
 const SignIn = () => {
   const [formData, setFormData]=useState({});
-  const [error, setError]=useState(null)
-  const [loading, setLoading]=useState(false)
+  const { loading, error } = useSelector((state) => state.user);
   const navigate=useNavigate();
+  const dispatch=useDispatch();
 
   const handleChange = (e)=>{
    setFormData(
@@ -14,41 +16,30 @@ const SignIn = () => {
    ) 
       console.log(formData)
   }
-  const handleSubmit= async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    
-    setLoading(true);
-    
-    try{
-      setLoading(true)
-      const res= await fetch('/api/auth/signin',
-      {
-        method:'POST',
-        headers:{
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData)
-      }
-      )
-      const data=await res.json();
-      if(data.success===false){
-        setError(data.message);
-        setLoading(false);
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false)
-      setError(null)
-      navigate('/about')
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
-    catch(error){
-      setLoading(false)
-      setLoading(error.message)
-
-    }
-   
-
-  }
+  };
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
