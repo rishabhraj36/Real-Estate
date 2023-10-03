bcbc926b9a9ff93659006ac370d0f98c63250d4e
rshvraj <rrishabh.flutter@gmail.com>
2023-10-04 00:03:14 +0530

    delete functionality working

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express=require("express");
-const { test,updateUser } = require("../controllers/user.controller");
+const { test,updateUser,deleteUser } = require("../controllers/user.controller");
 const {verifyToken}=require("../utils/verifyUser")
 const router=express.Router()
 
 router.get("/update",test)
 
 router.post('/update/:id',verifyToken,updateUser)
+router.delete('/delete/:id',verifyToken,deleteUser)
 module.exports=router
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -83,6 +86,24 @@ const Profile=()=> {
       dispatch(updateUserFailure(error.message));
     }
   };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -144,8 +165,12 @@ const Profile=()=> {
         </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+      <span
+          onClick={handleDeleteUser}
+          className='text-red-700 cursor-pointer'
+        >
+          Delete account
+        </span>        <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
 
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
