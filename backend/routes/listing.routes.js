e793c178be2aea1a97986ade16e5aa88e6a5eaea
rshvraj <rrishabh.flutter@gmail.com>
2023-10-04 17:59:45 +0530

    delete user listing functionality completed

--- backend/routes/listing.routes.js
+++ backend/routes/listing.routes.js
@@ -1,9 +1,10 @@
 const express=require("express");
-const { createListing } = require("../controllers/listing.controller");
+const { createListing,deleteListing } = require("../controllers/listing.controller");
 const { verifyToken } = require("../utils/verifyUser");
 const router=express.Router()
 
 
 router.post('/create',verifyToken ,createListing)
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 module.exports=router
