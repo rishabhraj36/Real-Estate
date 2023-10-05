9890eabff08936d685b8546f4d03ac8d9580623a
rshvraj <rrishabh.flutter@gmail.com>
2023-10-05 12:13:53 +0530

    search api route

--- backend/routes/listing.routes.js
+++ backend/routes/listing.routes.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const { createListing,deleteListing,updateListing, getListing } = require("../controllers/listing.controller");
+const { createListing,deleteListing,updateListing, getListing,getListings } = require("../controllers/listing.controller");
 const { verifyToken } = require("../utils/verifyUser");
 const router=express.Router()
 
@@ -8,5 +8,6 @@ router.post('/create',verifyToken ,createListing)
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 module.exports=router
